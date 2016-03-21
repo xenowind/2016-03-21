ed56c769a69f6a3c44520b92c7e90e79bba36c75
xenowind <xenowind@naver.com>
2016-03-21 11:31:46 +0900

    3rd

--- 2016-03-21.sql
+++ 2016-03-21.sql
@@ -23,3 +23,24 @@ SELECT
 FROM 
 	first_rental
 ;
+-----------------------------------
+DROP TEMPORARY TABLE IF EXISTS first_rental;
+CREATE TEMPORARY TABLE first_rental
+SELECT 
+	r.customer_id,
+	MIN(r.rental_date) first_time
+FROM
+	rental r
+GROUP BY 1
+;
+
+DROP TEMPORARY TABLE IF EXISTS cohort_size;
+CREATE TEMPORARY TABLE cohort_size
+SELECT
+	LEFT(first_time, 7),
+	COUNT(*)
+FROM
+	first_rental
+GROUP BY 1
+;	
+
