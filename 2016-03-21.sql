adcb993c8c1006db8bc99c2392f1b93e2966c40a
xenowind <xenowind@naver.com>
2016-03-21 11:48:32 +0900

    4st

--- 2016-03-21.sql
+++ 2016-03-21.sql
@@ -37,10 +37,24 @@ GROUP BY 1
 DROP TEMPORARY TABLE IF EXISTS cohort_size;
 CREATE TEMPORARY TABLE cohort_size
 SELECT
-	LEFT(first_time, 7),
-	COUNT(*)
+	LEFT(first_time, 7) month,
+	COUNT(*) num
 FROM
 	first_rental
 GROUP BY 1
 ;	
 
+SELECT
+	r.*,
+	LEFT(fr.first_time,7) cohort,
+	p.amount
+FROM
+	rental r,
+	first_rental fr,
+	cohort_size cs,
+	payment p
+WHERE
+	r.customer_id=fr.customer_id
+	AND cs.month=LEFT(fr.first_time,7)
+	AND p.rental_id = r.rental_id
+;
